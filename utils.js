e6130cb5330e2cb5d060b3539462b47c8ea8f593
Gabriel Cunha <gespcunha@gmail.com>
2020-03-01 14:42:13 +0000

    Fixed CLAN_LAST_WARS_URL

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ module.exports = function() {
     const CLAN_TAG = "9LU2Y8LU"
 
     const CLAN_URL = "https://cr-api-proxy.herokuapp.com/v1/clans/%23" + CLAN_TAG
-    const CLAN_LAST_WARS_URL = CLAN_URL + CLAN_TAG + "/warlog" // last 10 wars
+    const CLAN_LAST_WARS_URL = CLAN_URL + "/warlog" // last 10 wars
     const USERNAME = ""
     const PASSWORD = "Jj7UY.gu-QhC4npjZPhyxDgeE@hd8nzEX7HW"
     const AUTH = "Basic " + new Buffer.from(USERNAME + ":" + PASSWORD).toString("base64")
